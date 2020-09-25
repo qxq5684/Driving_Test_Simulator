15048d2cae31c3a1aac32710a198e32a07413ea4
Ben Williams <qxq5684@autuni.ac.nz>
2020-09-25 12:59:28 +1200

    Added more of map Logic for stopping at checkpoints for quiz questions

--- Assets/Mod Assets/ModResources/Scripts/Kart/SimpleTrigger.cs
+++ Assets/Mod Assets/ModResources/Scripts/Kart/SimpleTrigger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Packages.Rider.Editor.UnitTesting;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,7 +10,7 @@ public class SimpleTrigger : MonoBehaviour
     public Rigidbody triggerBody; 
     public UnityEvent onTriggerEnter;
 
-
+    public TestEvent testevent;
     void OnTriggerEnter(Collider other){
         //do not trigger if there's no trigger target object
         if (triggerBody == null) return;
